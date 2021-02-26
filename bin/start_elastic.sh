26b8e1632ae62122d1ea9f9167dde66860058348
Ben Marlin <marlin@cs.umass.edu>
2021-02-25 21:48:00 -0500

    separating ES and kibana startup

--- bin/start_elastic.sh
+++ bin/start_elastic.sh
@@ -21,31 +21,3 @@ echo "Elasticsearch available"
 
 # configure index template to correctly handle timestamps
 curl -H 'Content-Type: application/json' -XPUT http://localhost:9200/_template/mhl_template -d '{"order":0,"template":"*","settings":{},"mappings":{"auto":{"properties":{"timestamp":{"format":"epoch_millis","type":"date"}}}},"aliases":{}}'
-
-# start kibana
-echo 'Starting kibana'
-screen -d -m -S kb
-sleep 1s
-screen -S kb -p 0 -X stuff "${KIBANA_ROOT}/bin/kibana
-"
-
-echo 'Starting elastic tap'
-screen -d -m -S elastictap
-sleep 1s
-screen -S elastictap -p 0 -X stuff "cd ${MHL_ROOT}/apps/ElasticTap_jar; ./run.sh
-"
-
-echo "Checking Kibana status"
-echo $(curl -XGET localhost:5601) | grep '/app/kibana'
-
-while [ ! "$?" -eq 0 ]
-do
-	echo "Kibana not available.  Retrying."
-	sleep 2s
-	echo $(curl -XGET localhost:5601) | grep '/app/kibana'
-done
-
-echo "Kibana available"
-
-# import kibana dashboard
-curl -XPOST 'http://localhost:5601/api/kibana/dashboards/import' -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @"${MHL_ROOT}/config/dash.json"
---
+++ bin/start_kibana.sh
@@ -0,0 +1,37 @@
+
+source ~/.profile
+
+#Make sure elastic search is running 
+echo "Checking Elasticsearch status"
+echo $(curl -XGET localhost:9200) | grep 'mHealthLab'
+  
+while [ ! "$?" -eq 0 ]
+do
+	echo "Elasticsearch not available.  Retrying."
+	sleep 2s
+	echo $(curl -XGET localhost:9200) | grep 'mHealthLab'
+done
+
+echo "Elasticsearch available"
+
+# start kibana
+echo 'Starting kibana'
+screen -d -m -S kb
+sleep 1s
+screen -S kb -p 0 -X stuff "${KIBANA_ROOT}/bin/kibana
+"
+
+echo "Checking Kibana status"
+echo $(curl -XGET localhost:5601) | grep '/app/kibana'
+
+while [ ! "$?" -eq 0 ]
+do
+	echo "Kibana not available.  Retrying."
+	sleep 2s
+	echo $(curl -XGET localhost:5601) | grep '/app/kibana'
+done
+
+echo "Kibana available"
+
+# import kibana dashboard
+curl -XPOST 'http://localhost:5601/api/kibana/dashboards/import' -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @"${MHL_ROOT}/config/dash.json"
