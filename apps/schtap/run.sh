70ca88694559e4be9319fe7eecd24372aec5dd15
Ben Marlin <marlin@cs.umass.edu>
2021-02-25 21:30:02 -0500

    fixing sch run command

--- apps/schtap/run.sh
+++ apps/schtap/run.sh
@@ -1,4 +1,4 @@
 touch monitored_processes.txt
 
-java -jar SCHTap.jar localhost:9092,localhost:9093,localhost:9094 domino monitored_processes.txt '/nfs/obelix/users1/erisinger/projects/sch/stress-detection-active-learning-system/memphis_main.py --chunk_size 60' test 1
+java -jar $MHL_ROOT/apps/schtap/SCHTap.jar localhost:9092,localhost:9093,localhost:9094 domino monitored_processes.txt '/nfs/obelix/users1/erisinger/projects/sch/stress-detection-active-learning-system/memphis_main.py --chunk_size 60' test 1
 
