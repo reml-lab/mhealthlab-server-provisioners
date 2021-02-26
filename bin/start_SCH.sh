9afa496ebaa3b6fd191444fd48e6afaad16f063c
Ben Marlin <marlin@cs.umass.edu>
2021-02-25 21:17:05 -0500

    fixing sch screen command

--- bin/start_SCH.sh
+++ bin/start_SCH.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 echo "starting SCH"
 screen -d -m -S sch
 sleep 1s
-screen -S dcrs -p 0 -X stuff "$MHL_ROOT/apps/schtap/run.sh
+screen -S sch -p 0 -X stuff "$MHL_ROOT/apps/schtap/run.sh
 "
 echo "DCRS running at $(hostname -I) : 9595"
 
