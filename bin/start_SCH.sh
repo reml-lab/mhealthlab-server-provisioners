4f32cce57f7430160c4da8b2fdfa61869188fbcc
Ben Marlin <marlin@cs.umass.edu>
2021-02-25 21:20:40 -0500

    fixing sch screen command

--- bin/start_SCH.sh
+++ bin/start_SCH.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 echo "starting SCH"
 screen -d -m -S sch
 sleep 1s
-screen -S sch -p 0 -X stuff "$MHL_ROOT/apps/schtap/run.sh
+screen -S sch -p 0 -X stuff "bash $MHL_ROOT/apps/schtap/run.sh
 "
 echo "SCH running."
 
