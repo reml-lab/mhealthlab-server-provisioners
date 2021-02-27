31f9a9ed6ba54bb89c98e2066a201b9c56c7e808
Ben Marlin <marlin@cs.umass.edu>
2021-02-26 22:25:00 -0500

    added new options to control indexing frequency

--- apps/ElasticTap_jar/run.sh
+++ apps/ElasticTap_jar/run.sh
@@ -1,1 +1,1 @@
-java -jar $MHL_ROOT/apps/ElasticTap_jar/ObelixTap.jar localhost:9092 data-message,system-message,analytics-message domino
+java -jar $MHL_ROOT/apps/ElasticTap_jar/ObelixTap.jar localhost:9092 data-message,system-message,analytics-message domino 1000, 5000 
